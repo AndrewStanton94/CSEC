d432842e3b74c8265a171c0fc49b95808ad4bcd8
Andrew Stanton <up683693@myport.ac.uk>
2018-01-29 11:31:57 +0000

    Signed the lines

---
+++ MoodleFilesCWK1/massSign.sh
@@ -0,0 +1,12 @@
+for value in {1..10}
+do
+	echo $value
+	openssl dgst -sha256 -sign PrivateKeys/fourkey.pem -out lineHashes/signedhash-${value}-fourkey.txt "lines/line${value}v2.txt"
+done
+
+for value in {1..10}
+do
+	echo $value
+	openssl dgst -sha256 -sign PrivateKeys/twokey.pem -out lineHashes/signedhash-${value}-fourkey.txt "lines/line${value}v2.txt"
+done
+wc -l
